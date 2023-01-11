b6f6e2d84aef855d1f9f8abadb1fe7c6b22d62a9
Gene Boggs <gene@ology.net>
2023-01-11 08:15:11 -0800

    FIX SYNOPSIS!!!

--- lib/Chemistry/PeriodicTable.pm
+++ lib/Chemistry/PeriodicTable.pm
@@ -20,7 +20,7 @@ use namespace::clean;
 
   my $filename = $pt->as_file;
 
-  my @headers = $pt->header;
+  my $headers = $pt->header;
   my $symbols = $pt->symbols; # element properties keyed by symbol
 
   $pt->number('H');        # 1
